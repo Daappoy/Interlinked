b6f5534c2d85063a43ce3fd8ea66c1b3de362965
Daappoy <m.alghifaridaffa@gmail.com>
2024-09-17 11:48:51 +0700

    Pages navigation is improved, added with next and prev buttons

--- Assets/Script/TutorialPages.cs
+++ Assets/Script/TutorialPages.cs
@@ -6,63 +6,82 @@ using UnityEngine.UI;
 using System.Runtime.CompilerServices;
 public class TutorialPages : MonoBehaviour
 {
-    public Button PageOneButton;
-    public Button PageTwoButton;
-    public Button PageThreeButton;
-    public Button PageFourButton;
-    public Button PageFiveButton;
+    [Header("Buttons")]
+    // public Button PageOneButton;
+    // public Button PageTwoButton;
+    // public Button PageThreeButton;
+    // public Button PageFourButton;
+    // public Button PageFiveButton;
+    public GameObject next;
+    public GameObject prev;
+
+    [Header("Pages")]
     public GameObject Pageone;
     public GameObject Pagetwo;
     public GameObject Pagethree;
     public GameObject Pagefour;
     public GameObject Pagefive;
-    public void OnEnable()
+
+    [Header("dddddd")]
+    public int pageIndex =1;
+
+
+    public void NextPage()
     {
-        Pageone.SetActive(true);
-        Pagetwo.SetActive(false);
-        Pagethree.SetActive(false);
-        Pagefour.SetActive(false);
-        Pagefive.SetActive(false);
+        pageIndex ++;
     }
-    public void PageOne()
+    public void PrevPage()
     {
-        Pageone.SetActive(true);
-        Pagetwo.SetActive(false);
-        Pagethree.SetActive(false);
-        Pagefour.SetActive(false);
-        Pagefive.SetActive(false);
+        pageIndex --;
     }
-    public void PageTwo()
+    public void Update()
     {
-        Pageone.SetActive(false);
-        Pagetwo.SetActive(true);
-        Pagethree.SetActive(false);
-        Pagefour.SetActive(false);
-        Pagefive.SetActive(false);
+        if(pageIndex == 1)
+        {
+            prev.SetActive(false);
+            Pageone.SetActive(true);
+            Pagetwo.SetActive(false);
+            Pagethree.SetActive(false);
+            Pagefour.SetActive(false);
+            Pagefive.SetActive(false);
+        }
+        if(pageIndex == 2)
+        {
+            prev.SetActive(true);
+            Pageone.SetActive(false);
+            Pagetwo.SetActive(true);
+            Pagethree.SetActive(false);
+            Pagefour.SetActive(false);
+            Pagefive.SetActive(false);
+        }
+        if(pageIndex == 3)
+        {
+            Pageone.SetActive(false);
+            Pagetwo.SetActive(false);
+            Pagethree.SetActive(true);
+            Pagefour.SetActive(false);
+            Pagefive.SetActive(false);
+        }
+        if(pageIndex == 4)
+        {
+            next.SetActive(true);
+            Pageone.SetActive(false);
+            Pagetwo.SetActive(false);
+            Pagethree.SetActive(false);
+            Pagefour.SetActive(true);
+            Pagefive.SetActive(false);
+        }
+        if(pageIndex == 5)
+        {
+            next.SetActive(false);
+            Pageone.SetActive(false);
+            Pagetwo.SetActive(false);
+            Pagethree.SetActive(false);
+            Pagefour.SetActive(false);
+            Pagefive.SetActive(true);
+        }
     }
-    public void PageThree()
-    {
-        Pageone.SetActive(false);
-        Pagetwo.SetActive(false);
-        Pagethree.SetActive(true);
-        Pagefour.SetActive(false);
-        Pagefive.SetActive(false);
-    }
-    public void PageFour()
-    {
-        Pageone.SetActive(false);
-        Pagetwo.SetActive(false);
-        Pagethree.SetActive(false);
-        Pagefour.SetActive(true);
-        Pagefive.SetActive(false);
-    }
-    public void PageFive()
-    {
-        Pageone.SetActive(false);
-        Pagetwo.SetActive(false);
-        Pagethree.SetActive(false);
-        Pagefour.SetActive(false);
-        Pagefive.SetActive(true);
-    }
+
+
 
 }
